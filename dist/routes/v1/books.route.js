c746210a9091345a0d341b6aa5dbeb6459df185c
RedRe4per <s3582474@gmail.com>
2023-06-01 21:09:06 +1000

    fix: dist ignore

---
+++ dist/routes/v1/books.route.js
@@ -0,0 +1,73 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const uuid_1 = require("uuid");
+const user_1 = require("../../models/user");
+const router = (0, express_1.Router)();
+router.get('/', async (req, res) => {
+    try {
+        const users = await user_1.UserModel.scan().limit(50).exec();
+        res.status(200).json(users);
+    }
+    catch (error) {
+        console.error('An error ocurred:', error);
+        res.status(500).json(error);
+    }
+});
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await user_1.UserModel.get(id);
+        res.status(200).json(user);
+    }
+    catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({ error: 'Could not fetch user' });
+    }
+});
+router.post('/', async (req, res) => {
+    try {
+        const user = new user_1.UserModel({
+            id: (0, uuid_1.v4)(),
+            ...req.body,
+        });
+        user.save();
+        res.status(201).json({ message: 'new user created, 123', item: user });
+    }
+    catch (error) {
+        console.error('Error creating book:', error);
+        res.status(500).json({ error: 'Could not create book' });
+    }
+});
+router.put('/test', async (req, res) => {
+    try {
+        res.status(200).json({ message: 'test' });
+    }
+    catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'Could not update user' });
+    }
+});
+// router.put('/:id', async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const { name, email } = req.body;
+//   try {
+//     await UserModel.update({ id, name, email });
+//     res.status(200).json({ message: 'User updated' });
+//   } catch (error) {
+//     console.error('Error updating user:', error);
+//     res.status(500).json({ error: 'Could not update user' });
+//   }
+// });
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await user_1.UserModel.update({ id }, { isDeleted: true });
+        res.status(200).json({ message: 'User deleted' });
+    }
+    catch (error) {
+        console.error('An error occurred:', error);
+        res.status(500).json({ error: 'Could not delete user' });
+    }
+});
+exports.default = router;
