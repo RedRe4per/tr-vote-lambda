0dbb80e3054cc681269aed09e6f2010dd942a05c
RedRe4per <s3582474@gmail.com>
2023-06-01 17:03:35 +1000

    fix: prettier delete cr bug

--- dist/models/user.js
+++ dist/models/user.js
@@ -1,10 +1,10 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserModel = void 0;
-const dynamoose_1 = require('dynamoose');
+const dynamoose_1 = require("dynamoose");
 const UserSchema = new dynamoose_1.Schema({
-  id: String,
-  name: String,
-  email: String,
+    id: String,
+    name: String,
+    email: String,
 });
 exports.UserModel = (0, dynamoose_1.model)('User', UserSchema);
